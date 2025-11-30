39fceab8451b6af572537c20e947f368f85b2458
Andrew Wittrock <andy@wittrock.us>
2025-11-30 09:16:13 -0600

    Begin implementing layout system

--- index.tsx
+++ index.tsx
@@ -1,22 +1,26 @@
+import { ComponentRegistry } from "@core/lib/layout/componentRegistry";
 import { IModule } from "@core/lib/module";
-import { uacMenus } from "./lib/menus";
-import { uacRoutes } from "./lib/routes";
-import { IMyAccountTabProps, uacPlugins } from "./lib/plugin/slots";
-import { ProfileEditor } from "./components/ProfileEditor";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { AccountMenu } from "./components/AccountMenu";
+import { ProfileEditor } from "./components/ProfileEditor";
+import { uacMenus } from "./lib/menus";
+import { IMyAccountTabProps, uacPlugins } from "./lib/plugin/slots";
+import { uacRoutes } from "./lib/routes";
 import { uacSettings } from "./lib/settings";
 
-export const module:IModule = {
+export const module: IModule = {
     name: "uac",
     menus: uacMenus,
     routes: uacRoutes,
     settings: uacSettings,
 }
 
+ComponentRegistry.register("AccountMenu", AccountMenu, { category: "Layouts", displayName: "Account Menu" });
+
 uacPlugins.myAccount.tabs.register({
     key: "profile",
     title: "My Profile",
     icon: faUser,
     priority: 1000,
-    component: (props:IMyAccountTabProps) => <ProfileEditor {...props} title="My Profile" />,
+    component: (props: IMyAccountTabProps) => <ProfileEditor {...props} title="My Profile" />,
 });
